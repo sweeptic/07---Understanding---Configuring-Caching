02422bdbe9f2f89f30cf48be3e77da028f709c42
Attila Horvath <sweeptic@gmail.com>
2025-03-04 12:39:51 +0100

    request memoization in action

--- app/messages/page.js
+++ app/messages/page.js
@@ -1,11 +1,14 @@
 import Messages from '@/components/messages';
 
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages', {
-    headers: {
-      'X-ID': 'page',
-    },
-  });
+  const response = await fetch(
+    'http://localhost:8080/messages'
+    //    {
+    //     headers: {
+    //       'X-ID': 'page',
+    //     },
+    //   }
+  );
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
